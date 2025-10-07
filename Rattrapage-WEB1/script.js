9cbe11d7b7ed1f525084088b2cfff71f4ad2d94e
Astna17 <hei.antsa.17@gmail.com>
2025-10-07 09:43:40 +0300

    chore: add value

--- Rattrapage-WEB1/script.js
+++ Rattrapage-WEB1/script.js
@@ -1,20 +1,19 @@
-// document.getElementById('form')
-
 function ajouter() {
     const nomInput = document.getElementById('nom');
     const montantInput = document.getElementById('montant');
-    const categorieOption = document.getElementById('Catégorie')
+    const categorieOption = document.getElementById('categorie');
     const tableauCorps = document.getElementById('tableau').getElementsByTagName('tbody')[0];
 
-    const nom = nomInput.value;
-    const montant = montantInput.value;
+    const nom = nomInput.value.trim();
+    const montant = montantInput.value.trim();
     const categorie = categorieOption.value;
 
     if (nom && montant) {
         const nouvelleLigne = tableauCorps.insertRow();
+
         const celluleNom = nouvelleLigne.insertCell(0);
         const celluleMontant = nouvelleLigne.insertCell(1);
-        const celluleCategorie = nouvelleLigne.insertCell(1);
+        const celluleCategorie = nouvelleLigne.insertCell(2);
 
         celluleNom.textContent = nom;
         celluleMontant.textContent = montant;
@@ -22,7 +21,9 @@ function ajouter() {
 
         nomInput.value = '';
         montantInput.value = '';
+        categorieOption.selectedIndex = 0;
+
     } else {
-        alert("")
+        alert("Veuillez remplir tous les champs.");
     }
 }
